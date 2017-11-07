0905fff6d14ec2199f49328a120cf62fe7ae602b
Chengyu <1529514910@qq.com>
2017-11-06 23:28:14 -0800

    New comments~

--- 236. Lowest Common Ancestor of a Binary Tree/lowestCommonAncestor.java
+++ 236. Lowest Common Ancestor of a Binary Tree/lowestCommonAncestor.java
@@ -8,27 +8,24 @@
  * }
  */
 class Solution {
-    //if p and q seperate in twoo diff branches, return root.
-    //o/w we only need to check one sub-branch recursively.
-    //when either p or q is the answer, we will output p/q directly.
+    //Solve this question by recursion
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q){
+        if (root == null || root == p || root == q)
             return root;
-        }
         else{
             TreeNode left = lowestCommonAncestor(root.left, p, q);
             TreeNode right=lowestCommonAncestor(root.right, p, q);
-            //p and q are in different branches
-            if(left != null && right != null){
+            
+            if(left == null && right == null){
+                return null;
+            }
+            if(left == null && right != null){
+                return right;
+            }
+            if(left != null && right == null)
+                return left;
+            else
                 return root;
-            }
-            else{
-                if(left == null){
-                    return right;
-                }
-                else
-                    return left;
-            }
         }
     }
 }
